1330dc2a2975315efc04bff461bd98a7f2fb4a69
sanghyeon1225 <132208851+sanghyeon1225@users.noreply.github.com>
2024-07-12 02:02:47 +0900

    [Silver V] Title: 다리 놓기, Time: 220 ms, Memory: 18404 KB -BaekjoonHub

--- 백준/Silver/1010. 다리 놓기/다리 놓기.java
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.java
@@ -1,26 +1,21 @@
+
 import java.util.Scanner;
 
-public class Main {
-    public static void main(String[] args) {
+public class Main{
+    public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int time = sc.nextInt();
-        for (int i = 0; i < time; i++) {
+        for(int i = 0 ; i < time ; i++){
             int a = sc.nextInt();
             int b = sc.nextInt();
-            System.out.println(combination(b, a));
+            long result = 1;
+            
+            for(int j = 0; j < a; j++){
+                result *= (b-j);
+                result /= (j+1);
+            }
+            
+            System.out.println(result);
         }
-    }
-
-    // 조합을 계산하는 함수
-    public static int combination(int n, int r) {
-        if (r > n - r) {
-            r = n - r; // 더 작은 r을 선택하여 계산을 최적화합니다.
-        }
-        long result = 1; // 중간 계산이 커질 수 있으므로 long 사용
-        for (int i = 0; i < r; i++) {
-            result *= (n - i);
-            result /= (i + 1);
-        }
-        return (int) result;
     }
 }
