9c551bd1786608bbd1b7cfc2f15b97575ae80e0b
sanghyeon1225 <132208851+sanghyeon1225@users.noreply.github.com>
2024-07-11 23:27:53 +0900

    [Bronze V] Title: 나머지, Time: 176 ms, Memory: 17684 KB -BaekjoonHub

--- 백준/Bronze/10430. 나머지/나머지.java
+++ 백준/Bronze/10430. 나머지/나머지.java
@@ -3,12 +3,12 @@ import java.util.Scanner;
 public class Main{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        int A = sc.nextInt();
-        int B = sc.nextInt();
-        int C = sc.nextInt();
-        System.out.println( (A+B)%C );
-		System.out.println( (A%C + B%C)%C );
-		System.out.println( (A*B)%C );
-		System.out.println( (A%C * B%C)%C );
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        System.out.println((a + b) % c);
+        System.out.println((a % c + b % c) % c);
+        System.out.println((a * b) % c);
+        System.out.println((a % c * b % c) % c);
     }
 }
