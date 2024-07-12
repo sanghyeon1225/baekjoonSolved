86f9b44533b094c696f003de6c30acd8941b00d4
sanghyeon1225 <132208851+sanghyeon1225@users.noreply.github.com>
2024-07-13 03:20:08 +0900

    [Bronze V] Title: 문자열, Time: 160 ms, Memory: 17540 KB -BaekjoonHub

--- 백준/Bronze/9086. 문자열/문자열.java
+++ 백준/Bronze/9086. 문자열/문자열.java
@@ -6,8 +6,9 @@ public class Main{
         int a = s.nextInt();
         for(int i = 0; i < a; i++){
             String s1 = s.next();
-            System.out.print(s1.substring(0,1));
-            System.out.println(s1.substring(s1.length()-1, s1.length()));
+            System.out.print(s1.charAt(0));
+            System.out.println(s1.charAt(s1.length()-1));
         }
+        
     }
 }
