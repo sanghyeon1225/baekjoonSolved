1a380449055433d49f6c125f82baa05cff5ea504
sanghyeon1225 <132208851+sanghyeon1225@users.noreply.github.com>
2025-02-04 09:36:05 +0900

    [Silver II] Title: 부분수열의 합, Time: 628 ms, Memory: 82416 KB -BaekjoonHub

---
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
@@ -0,0 +1,15 @@
+import sys
+from itertools import combinations
+
+n, s = map(int, input().split()) # 수열 속 정수의 개수 n, 타겟 넘버 s 입력 받기
+number = list(map(int, input().split())) # 수열 값 입력 받기
+answer = 0
+
+for i in range(n):
+    sub = list(combinations(number, i+1)) # combinations 함수를 이용하여 부분수열을 구함
+    for j in range(len(sub)): 
+        total = sum(sub[j]) 
+        if s == total: # 부분수열의 합과 타겟 넘버가 같은지 확인
+            answer = answer + 1
+                        
+print(answer)
