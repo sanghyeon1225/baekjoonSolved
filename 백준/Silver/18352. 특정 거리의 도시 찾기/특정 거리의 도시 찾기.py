9b7e31444c811dc5ddf1feb6d2b0d460f975dd11
sanghyeon1225 <132208851+sanghyeon1225@users.noreply.github.com>
2025-02-16 22:26:16 +0900

    [Silver II] Title: 특정 거리의 도시 찾기, Time: 1832 ms, Memory: 100784 KB -BaekjoonHub

--- 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.py
+++ 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.py
@@ -1,34 +1,31 @@
 from collections import deque
 import sys
 
-# 입력
-# n, m, k, s = map(int, input().split())
-n, m, k, s = list(map(int, sys.stdin.readline().split()))
+def BFS(graph, start, visited):
+    queue = deque([start])
+    visited[start] = 0
+    while queue:
+        v = queue.popleft()
+        for i in graph[v]:
+            if visited[i] == -1:
+                visited[i] = visited[v] + 1
+                queue.append(i)
+            
+n, m, k, x = map(int, sys.stdin.readline().split())
+graph = [[] for _ in range(n + 1)]
+visited = [-1] * (n + 1)
 
-graph = {}
-for i in range(n+1):
-    graph[i] = []
 for i in range(m):
-    a, b = list(map(int, sys.stdin.readline().split()))
+    a, b = map(int, sys.stdin.readline().split())
     graph[a].append(b)
 
-# bfs
-dist_list = [0 for _ in range(n + 1)]
-visited = [0 for _ in range(n+1)]
-queue = deque([s])
-# queue.append(s)
-visited[s] = 1
-while (queue):
-    now = queue.popleft()
-    for j in graph[now]:
-        if visited[j] == 0:
-            queue.append(j)
-            visited[j] = 1
-            dist_list[j] = dist_list[now] + 1
-check = 0
-for i in range(1, n+1):
-    if dist_list[i] == k:
+BFS(graph, x, visited)
+
+count = 0
+for i in range(n+1):
+    if visited[i] == k:
         print(i)
-        check += 1
-if check == 0:
+        count += 1
+        
+if (count == 0):
     print(-1)
