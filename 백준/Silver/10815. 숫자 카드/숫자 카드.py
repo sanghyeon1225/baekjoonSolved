8d4cf5f73a7de6936c78db7849893987e1fc8d4f
이상현 <132208851+sanghyeon1225@users.noreply.github.com>
2025-05-29 14:41:17 +0900

    [Silver V] Title: 숫자 카드, Time: 496 ms, Memory: 119324 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.py
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.py
@@ -1,26 +1,9 @@
-def binary_search(target, data, start, end):
-    if start > end:
-        return 0
-    mid = (start + end) // 2
-    
-    if data[mid] == target:
-        return 1
-    elif data[mid] > target:
-        end = mid - 1
-    else:
-        start = mid + 1
-    
-    return binary_search(target, data, start, end)
-
 n = int(input())
-card = list(map(int, input().split()))
-card.sort()
+card = set(map(int, input().split()))  # set으로 변환
 
 m = int(input())
 find_card = list(map(int, input().split()))
 
-answer = []
-for i in range(m):
-    answer.append(binary_search(find_card[i], card, 0, n-1))
+answer = [1 if x in card else 0 for x in find_card]
 
 print(*answer)
