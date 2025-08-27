c3a61d250c785df48c3b123b8ba91b78887cb056
이상현 <132208851+sanghyeon1225@users.noreply.github.com>
2025-08-28 05:35:38 +0900

    [Gold V] Title: 탑, Time: 676 ms, Memory: 144344 KB -BaekjoonHub

---
+++ 백준/Gold/2493. 탑/탑.py
@@ -0,0 +1,20 @@
+n = int(input())
+
+tower = list(map(int, input().split()))
+
+stack = []
+answer = []
+
+for i in range(n):
+    while stack:
+        if stack[-1][1] > tower[i]: 
+            answer.append(stack[-1][0] + 1)
+            break
+        else:
+            stack.pop()
+
+    if not stack:
+        answer.append(0)
+    stack.append([i, tower[i]])
+
+print(*answer)
