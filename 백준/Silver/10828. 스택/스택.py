e90a3d17996dfee2b4417e5617ba7efe08a60fb7
이상현 <132208851+sanghyeon1225@users.noreply.github.com>
2025-08-27 13:34:32 +0900

    [Silver IV] Title: 스택, Time: 44 ms, Memory: 33432 KB -BaekjoonHub

---
+++ 백준/Silver/10828. 스택/스택.py
@@ -0,0 +1,21 @@
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+
+stack = []
+
+for i in range(n):
+    command = input().split()
+    if command[0] == "push":
+        stack.append(command[1])
+    elif command[0] == "pop":
+        print(stack.pop() if stack else -1)
+    elif command[0] == "size":
+        print(len(stack))
+    elif command[0] == "empty":
+        print(0 if stack else 1)
+    elif command[0] == "top":
+        print(stack[-1] if stack else -1)
+        
