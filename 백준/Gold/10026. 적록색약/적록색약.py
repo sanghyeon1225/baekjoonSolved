a315958ed9ee00073136863e870d16cdf2d0ef4c
이상현 <132208851+sanghyeon1225@users.noreply.github.com>
2025-09-05 17:14:01 +0900

    [Gold V] Title: 적록색약, Time: 68 ms, Memory: 35004 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.py
+++ 백준/Gold/10026. 적록색약/적록색약.py
@@ -17,25 +17,7 @@ def bfs(i, j):
             if (n > nx >= 0 and n > ny >= 0 and not visited[nx][ny] and data[nx][ny] == color):
                 q.append((nx, ny, color))
                 visited[nx][ny] = True
-        
-def bfs2(i, j):
-    q = deque([(i, j, data[i][j])])
-    visited[i][j] = True
-    
-    while(q):
-        x, y, color = q.popleft()
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            
-            if (n > nx >= 0 and n > ny >= 0 and not visited[nx][ny]):
-                if color in ("R", "G") and data[nx][ny] in ("R", "G"):
-                    q.append((nx, ny, color))
-                    visited[nx][ny] = True
-                elif color == "B" and data[nx][ny] == "B":
-                    q.append((nx, ny, color))
-                    visited[nx][ny] = True
-                    
+
 n = int(sys.stdin.readline())
 data = [list(sys.stdin.readline().rstrip()) for _ in range(n)]
 visited = [[False] * n for _ in range(n)]
@@ -52,8 +34,13 @@ visited = [[False] * n for _ in range(n)]
 
 for i in range(n):
     for j in range(n):
+        if data[i][j] == 'G':
+            data[i][j] = 'R'
+            
+for i in range(n):
+    for j in range(n):
         if not visited[i][j]:
-            bfs2(i, j)
+            bfs(i, j)
             count2 += 1
             
 print(count, count2)
