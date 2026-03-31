cd42bbceb91bded46f1a39790056cd89725600ab
이상현 <132208851+sanghyeon1225@users.noreply.github.com>
2026-03-31 11:24:38 +0900

    [Gold V] Title: 평범한 배낭, Time: 4264 ms, Memory: 230376 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -1,19 +1,12 @@
 n, k = map(int, input().split())
 
-data = [list(map(int, input().split())) for _ in range(n)]
-
-data.insert(0, [0,0])
-
-dp = [[0] * (k + 1) for _ in range(n + 1)]
+dp = [[0] * (k+1) for _ in range(n+1)]
 
 for i in range(1, n+1):
+    w, v = map(int, input().split())
     for j in range(1, k+1):
-        weight = data[i][0]
-        value = data[i][1]
-        
-        if j < weight:
-            dp[i][j] = dp[i-1][j]
+        if w <= j: 
+            dp[i][j] = max(dp[i-1][j], dp[i-1][j-w] + v)
         else:
-            dp[i][j] = max(dp[i-1][j], value + dp[i-1][j-weight])
-            
-print(dp[n][k])
+            dp[i][j] = dp[i-1][j]
+print(max(dp[-1]))
